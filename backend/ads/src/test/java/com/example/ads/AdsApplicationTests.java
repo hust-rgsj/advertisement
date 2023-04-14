9aad9c31add144ca614a4f5dc0ddd1ed24b818ba
coverupforV <pcw760404931@163.com>
2023-04-14 20:55:33 +0800

    new file:   backend/ads/.gitignore 	new file:   backend/ads/.mvn/wrapper/maven-wrapper.jar 	new file:   backend/ads/.mvn/wrapper/maven-wrapper.properties 	new file:   backend/ads/mvnw 	new file:   backend/ads/mvnw.cmd 	new file:   backend/ads/pom.xml 	new file:   backend/ads/src/main/java/com/example/ads/AdsApplication.java 	new file:   backend/ads/src/main/java/com/example/ads/Controller/UserController.java 	new file:   backend/ads/src/main/resources/application.yml 	new file:   backend/ads/src/test/java/com/example/ads/AdsApplicationTests.java

---
+++ backend/ads/src/test/java/com/example/ads/AdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
