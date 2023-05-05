ec8db753b7fbbedd4e8d205e243738d29e226831
Ray <18924127468@163.com>
2023-05-05 21:53:02 +0800

    feat: update topbar

--- fe/src/pages/CList/AdvContainer/index.tsx
+++ fe/src/pages/CList/AdvContainer/index.tsx
@@ -4,25 +4,30 @@ import AdvCard from "@/components/advCard";
 import useGetAdvList from "@/hooks/useGetAdvList";
 
 const AdvContainer = () => {
-  // const advList: Array<AdvCardData> = [
-  //   {
-  //     id: 11,
-  //     title: "广告标题",
-  //     description: "广告摘要广告摘要广告摘要广告摘要广告摘要",
-  //     img: "https://cdn.ray-blog.top/t01e900c82ac25cb7e2.jpg",
-  //     startTime: null,
-  //     endTime: null,
-  //     status: 11,
-  //   },
-  // ];
+  const advList: Array<AdvCardData> = [
+    {
+      id: 11,
+      title: "广告标题",
+      description: "广告摘要广告摘要广告摘要广告摘要广告摘要",
+      img: "https://cdn.ray-blog.top/t01e900c82ac25cb7e2.jpg",
+      startTime: null,
+      endTime: null,
+      status: 11,
+    },
+  ];
+  const loading = false;
 
-  const { advList, loading } = useGetAdvList();
+  // const { advList, loading } = useGetAdvList();
 
   return (
     <div className="adv-container">
-      {advList?.map((item, index) => {
-        return <AdvCard advCardData={item} key={index}></AdvCard>;
-      })}
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        advList?.map((item, index) => {
+          return <AdvCard advCardData={item} key={index}></AdvCard>;
+        })
+      )}
     </div>
   );
 };
