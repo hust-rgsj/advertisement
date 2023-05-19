f3d56e433d2e67ba94af2800fcf89e90a22aeedf
Hang <1767390619@qq.com>
2023-05-19 15:08:24 +0800

    docs: update

--- be/src/main/java/com/example/be/common/Status.java
+++ be/src/main/java/com/example/be/common/Status.java
@@ -1,23 +1,27 @@
 package com.example.be.common;
 
 public class Status {
+    // 用户状态 0 1
     public static final int BANNED = 0;
 
     public static final int RUNNING = 1;
+// 广告审核
+    public static final int EXAMING = 12;
 
-    public static final int EXAMING = 10;
+    public static final int PASS = 22;
 
-    public static final int NOT_PASS = 11;
+    public static final int NOT_PASS = 32;
 
-    public static final int PASS = 12;
+    //支付
+    public static final int NOT_PAID = 322;
 
-    public static final int ON = 100;
+    public static final int PAID = 222;
 
-    public static final int OFF = 101;
+// 广告部署
+    public static final int ON = 2222;
 
-    public static final int STOP =102;
+    public static final int OFF = 3222;
 
-    public static final int NOT_PAID = 1000;
+    public static final int STOP = 1222;
 
-    public static final int PAID = 1001;
 }
